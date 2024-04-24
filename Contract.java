62a25b024a5fd772d83a538fe64cef5ce6f4cfab
Abi Rusalavitch <arusalavitch@smith.edu>
2024-04-23 21:15:35 -0400

    sync

--- Contract.java
+++ Contract.java
@@ -1,5 +1,19 @@
 public interface Contract {
 
-
-
+    void startGame();
+    void inventory();
+    void grab(String item);
+    String drop(String item);
+    void sniff(String item);
+    void use(String item);
+    boolean scurry(String direction);
+    boolean jump(int x, int y);
+    Number eat();
+    Number poop();
+    void snooze();
+    void undo();
+    void solvePuzzle(String puzzleName, String solution);
+    void unlockAchievement(String achievementName);
+    void quitGame();
 }
+    
